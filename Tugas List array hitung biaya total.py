e08df43ccd13fc3316a032833729a23684bb81fe
zara cookie <85876952+zahrah-ambar@users.noreply.github.com>
2021-06-20 21:39:59 +0700

    Update Tugas List array hitung biaya total.py

--- Tugas List array hitung biaya total.py
+++ Tugas List array hitung biaya total.py
@@ -2,39 +2,37 @@
 zahrah ambar sari
 20083000124
 '''
-kode =['A','B']
 
+lg="y"
+while lg=="y":
+    
+    kode =['a','b']
+    kota = ['surabaya','bandung']
+    jarak = [169,452]
+    ongkirperkm = [2500,4000]
+    print("ekspedisi lorena")
+    print("kode kota tujuan dari: ")
+    print("a. malang -> surabaya ")
+    print("b. malang -> bandung ")
+    
+    pilihan = input(">> Masukkan Kode Tujuan = ")
+    if pilihan=="a":
+        idx = 0
+    elif pilihan=="b":
+        idx = 1
+    else:
+        idx = 0
 
-kota = ['Surabaya','Bandung']
-jarak = [169,452]
-ongkirperkm = [2500,4000]
+    print(">>> Pilihan Tujuan = " + kota[idx])
+    print(">>> Jarak          = " + str(jarak[idx]) + " km")
+    print(">>> Ongkir per km  = Rp. " + str(ongkirperkm[idx]))
 
+    fixjarak = jarak[idx]
+    fixongkirkm = ongkirperkm[idx]
+    totongkir = fixjarak * fixongkirkm
 
-print ("=============================================")
-print(" TRANSAKSI BIAYA EKSPEDISI ")
-print ("=============================================")
-print(" Kode = A. ",kota[0])
-print(" Kode = B. ",kota[1])
-print ("=============================================")
-
-pilihan = input(">> Masukkan Kode Tujuan = ")
-
-if pilihan=="a":
-    idx = 0
-elif pilihan=="b":
-    idx = 1
-else:
-    idx = 0
-
-
-print("Pilihan Tujuan = " + kota[idx])
-print("Jarak          = " + str(jarak[idx]) + " km")
-print("Ongkir per km  = Rp. " + str(ongkirperkm[idx]))
-
-
-fixjarak = jarak[idx]
-fixongkirkm = ongkirperkm[idx]
-totongkir = fixjarak * fixongkirkm
-
-print("-------------------------------------")
-print("Total Biaya     = Rp." + str(totongkir))
+    print(">>>-------------------------------------")
+    print(">>> Total Biaya     = Rp." + str(totongkir))
+    lg=input("coba lagi ? y/t.  ")
+    if lg=="t":
+        break 
